6965d8d2142c7953cff2fc0ec132fa6bcf7f962f
Coffeesw <897884964@qq.com>
2024-04-13 00:05:15 +0800

    ✨ feat: 更新配置

--- docs/docs/.vitepress/config.js
+++ docs/docs/.vitepress/config.js
@@ -2,18 +2,20 @@ module.exports = {
     title: 'Go语言学习之旅',
     description: '这里是Coffee的Go语言学习之旅',
     themeConfig: {
+        logo: '/golang.png',
+        nav: [
+            { text: 'Changelog', link: 'https://github.com/...' }
+        ],
         sidebar: [
-            {
-                text: '写在前面',
-                link:'/'
-            },
             {
                 text: '基本',
                 items: [
                     { text: 'go语言介绍', link: '/基本/go语言介绍' },
                     { text: '环境安装', link: '/基本/环境安装' },
+                    { text: '基本概念', link: '/基本/基本概念' }
                 ],
             }
         ]
-    }
+    },
+    lastUpdated: true
 }
--- docs/docs/index.md
+++ docs/docs/index.md
@@ -1,3 +1,32 @@
-# 写在前面
+---
+title: Go语言学习之旅
+description: 这里是Coffee的Go语言学习之旅
+editLink: true
+layout: home
 
-这里是Coffee的Go语言学习笔记
+hero:
+  name: Go语言学习之旅
+  text: 由浅入深学习Golang
+  tagline: 使用VitePress构建...
+  image:
+    src: /golang.png
+    alt: 开始吧！
+  actions:
+    - theme: brand
+      text: 开始
+      link: /基本/go语言介绍
+    - theme: alt
+      text: GitHub
+      link: https://github.com/vuejs/vitepress
+
+features:
+  - icon: 🛠️
+    title: 语言特性
+    details: 这里有我对Go语言的理解...
+  - icon: 😊
+    title: 问题记录
+    details: 这里有我遇到的问题，以及我的解决过程...
+  - icon: 🍎
+    title: 随心感想
+    details: 我写了好多...
+---
